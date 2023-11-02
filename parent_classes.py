0db41033a1af4deff1b98bf44e9bdca55cb3874a
myrrhis <148441013+myrrhis@users.noreply.github.com>
2023-11-02 14:13:21 +0300

    Update parent_classes.py file

--- parent_classes.py
+++ parent_classes.py
@@ -10,7 +10,6 @@ class Money:
     def ban_on_use(self, resolution=True):
         self.ban = resolution
 
-
 class Cash(Money):
 
     def __init__(self, type_of_currency, ratio_to_GBT, amount_of_currency, relevance):
@@ -21,7 +20,6 @@ class Cash(Money):
 
     def should_invest(self, resolution=True):
         self.should_invest = resolution
-
 
 class NonCashMoney(Money):
 
@@ -35,40 +33,40 @@ class NonCashMoney(Money):
 
 class Wallet:
 
-	def __init__(self, money):
-		self.money = money
+    def __init__(self, money):
+        self.money = money
 
-	def pay(self, amount):
-		money.amount_of_currency -= amount
-		return f'Из кошелька было взято денег на сумму {amount}'
+    def pay(self, amount):
+        money.amount_of_currency -= amount
+        return f'Из кошелька было взято денег на сумму {amount}'
 
-	def put_money(self, amount)
-		money.amount_of_currency += amount
-		return f'Кошелёк пополнен на сумму {amount}'
+    def put_money(self, amount)
+        money.amount_of_currency += amount
+        return f'Кошелёк пополнен на сумму {amount}'
 
 
 class CashWallet(Wallet):
 
-	def __init__(self, money, discont_cards):
-		super().__init__(money)
-		self.discont_cards = discont_cards
+    def __init__(self, money, discont_cards):
+        super().__init__(money)
+        self.discont_cards = discont_cards
 
-	def add_discont_cards(discont_card):
-		self.discont_cards.append(discont_card)
-		return f'У вас новая дисконтная карта: {discont_cards}!'
+    def add_discont_cards(discont_card):
+        self.discont_cards.append(discont_card)
+        return f'У вас новая дисконтная карта: {discont_cards}!'
 
 
 class NonCashWallet(Wallet):
 
-	def send_email(self, amount)
-		return f'Ваш чек по операции на сумму {amount}'
+    def send_email(self, amount)
+        return f'Ваш чек по операции на сумму {amount}'
 
-	def pay(self, amount):
-		money.amount_of_currency -= amount
-		self.send_email(amount)
-		return f'Из кошелька было взято денег на сумму {amount}'
+    def pay(self, amount):
+        money.amount_of_currency -= amount
+        self.send_email(amount)
+        return f'Из кошелька было взято денег на сумму {amount}'
 
-	def put_money(self, amount)
-		money.amount_of_currency += amount
-		self.send_email(amount)
-		return f'Кошелёк пополнен на сумму {amount}'
+    def put_money(self, amount)
+        money.amount_of_currency += amount
+        self.send_email(amount)
+        return f'Кошелёк пополнен на сумму {amount}'
